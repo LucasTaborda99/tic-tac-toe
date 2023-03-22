8517fb6bf02f6f5213bb43d2e0c3f66a91e72d11
lucas.taborda <lucas.taborda@fertipar.com.br>
2023-03-22 14:13:15 -0300

    Edited route player, added readPlayer route, type post

--- routes/player.js
+++ routes/player.js
@@ -3,8 +3,20 @@ const connection = require('../connection')
 const router = express.Router()
 require('dotenv').config()
 
-router.get('/player', (req, res) => {
-
+router.post('/readPlayer', (req, res) => {
+    const player = req.body
+    let query = 'SELECT * FROM players WHERE nickname = ? and password = ?'
+    connection.query(query, [player.nickname, player.password], (err, results) => {
+        if(!err) {
+            if(results.length <= 0) {
+                return res.status(400).json({message: 'Nickname ou password incorreto'})
+            } else {
+                return res.status(200).json(results[0])
+            }
+        } else {
+            return res.status(500).json({message: 'Erro ao tentar buscar player'})
+        }
+    })
 })
 
 module.exports = router
