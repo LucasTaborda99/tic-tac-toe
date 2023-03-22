837e454699653c77fdc59aae1b607b2d4618102b
lucas.taborda <lucas.taborda@fertipar.com.br>
2023-03-22 11:16:51 -0300

    Created folder routes and created route player

---
+++ routes/player.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const connection = require('../connection')
+const router = express.Router()
+require('dotenv').config()
+
+router.get('/player', (req, res) => {
+
+})
+
+module.exports = router
