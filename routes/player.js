6ccf8a243866cb985a831bbb4fb71f37b89a2317
lucas.taborda <lucas.taborda@fertipar.com.br>
2023-03-24 15:26:20 -0300

    Added route /createPlayer to create a new player
    Not working yet

--- routes/player.js
+++ routes/player.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const connection = require('../connection')
 const router = express.Router()
@@ -27,12 +28,35 @@ router.get('/getPlayers', (req, res) => {
             if(results.length <= 0) {
                 return res.status(400).json({message: 'Nenhum player encontrado'})
             } else {
-                return res.status(200).json(results)
+                return res.status(200).json(results[0])
             }
         } else {
             return res.status(500).json({message: 'Erro ao tentar buscar players'})
         }
     } )
+})
+
+router.post('/createPlayer', (req, res) => {
+    const player = req.body
+    query = 'SELECT email, nickname FROM players'
+    connection.query(query, [player.email, player.nickname], (err, results) => {
+        if(!err) {
+            if(results.length <= 0) {
+                let query = 'INSERT INTO players (name, nickname, email, password, status) VALUES (?, ?, ?, ?, "user")'
+                connection.query(query, [player.name, player.nickname,player.email, player.password], (err, results) => {
+                    if(!err) {
+                        return res.status(200).json({message: 'Player criado com sucesso'})
+                    } else {
+                        return res.status(500).json({message: 'Erro ao criar novo player'})
+                    }
+                })
+            } else {
+                return res.status(500).json({message: 'Email ou nickname já existentes'})
+            }
+        } else {
+            return res.status(500).json({message: 'Ops! Algo deu errado. Por favor, tente novamente mais tarde'})
+        }
+    })
 })
 
 module.exports = router
